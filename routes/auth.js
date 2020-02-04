74bf1230f0089de6ed48ff51f3fce5f1d91c8195
vivekgondalia <vgondalia@drintl.com>
2020-02-04 15:08:24 -0500

    email already exists

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,12 @@ router.post('/register', async (req, res) => {
     //validate prior to DB save
     const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
-    
+
+    //Check if user exist
+    const emailExists = await User.findOne({email: req.body.email});
+    if(emailExists) return res.status(400).send('Email already exists');
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
