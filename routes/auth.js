3c95b00402eae84aedf88a5f84deea7ff7eba098
vivekgondalia <vgondalia@drintl.com>
2020-02-04 14:57:30 -0500

    Joi validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//VALIDATION
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required().label("Name"),
+    email: Joi.string().min(6).required().email().label("Email"),
+    password: Joi.string().min(6).required().label("Password")
+})
+
 router.post('/register', async (req, res) => {
+    //VALIDATE DATA BEFORE SAVING A USER
+    const {error} = schema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
