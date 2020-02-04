5120d868b08ac5022a149c3ff117fb63d530ea78
vivekgondalia <vgondalia@drintl.com>
2020-02-04 16:29:08 -0500

    POST route set up

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const verify = require('./verifyToken');
+//const Post = require('../model/Post');
 
+//GET
 router.get('/', verify, (req, res) => {
     res.json({
         posts:{
@@ -10,5 +12,24 @@ router.get('/', verify, (req, res) => {
         }
     });
 });
+
+router.get('/all', verify, (req, res) => {
+    res.send('All POSTS');
+});
+
+// POST
+// router.post('/', verify, async (req, res) => {
+//     const post = new Post({
+//         title: req.body.title,
+//         description: req.body.description
+//     });
+
+//     try {
+//         const savedPost = await post.save();
+//         res.send(savedPost);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// });
 
 module.exports = router;
